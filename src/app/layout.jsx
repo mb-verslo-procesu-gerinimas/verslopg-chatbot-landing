fe682eec80c3d6d736308381c6a4a71f60ec19c4
augustinasn <47611976+augustinasn@users.noreply.github.com>
2025-07-05 22:23:15 +0300

    fixes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={clsx(
-        'h-full scroll-smooth bg-white antialiased',
+        'scroll-smooth bg-white antialiased',
         inter.variable,
         lexend.variable,
       )}
