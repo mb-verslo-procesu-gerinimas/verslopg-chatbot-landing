aab908bbed6f828be980b5ac797ee94ec2963067
augustinasn <47611976+augustinasn@users.noreply.github.com>
2025-07-06 22:35:54 +0300

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -11,9 +11,9 @@ export function Logo({ className, ...props }) {
       <div
         className={`${righteous.className} rounded-md bg-red-600 px-2 py-1 text-white`}
       >
-        ERPrompt.lt
+        ER
       </div>
-      {/* <span className="ml-2 font-medium">Prompt.lt</span> */}
+      <span className="ml-2 font-medium">Prompt.lt</span>
     </div>
   )
 }
