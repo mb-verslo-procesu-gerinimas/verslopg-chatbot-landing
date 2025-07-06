f095849160f7c60001d53d401cb7e47c4154fcfc
augustinasn <47611976+augustinasn@users.noreply.github.com>
2025-07-06 14:56:58 +0300

    logo updated

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -9,11 +9,11 @@ export function Logo({ className, ...props }) {
   return (
     <div className={`flex items-center text-2xl ${className}`} {...props}>
       <div
-        className={`${righteous.className} rounded-md bg-red-600 pl-2 pr-0 py-1 text-white`}
+        className={`${righteous.className} rounded-l-md bg-red-600 pl-2 pr-0 py-1 text-white`}
       >
         ERP
       </div>
-      <span className="ml-0 font-medium">rompt</span>
+      <span className="ml-0 font-medium">rompt.lt</span>
     </div>
   )
 }
