446029c374a5c7b36a3389530e1a3d947c765e0f
augustinasn <47611976+augustinasn@users.noreply.github.com>
2025-07-26 22:23:34 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -78,7 +78,6 @@ function MobileNavigation() {
         <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
         <MobileNavLink href="#pricing">Pricing</MobileNavLink> */}
         <MobileNavLink href="https://app.erprompt.lt">Prisijungti</MobileNavLink>
-        <MobileNavLink href="https://app.erprompt.lt">Registruotis</MobileNavLink>
       </PopoverPanel>
     </Popover>
   )
